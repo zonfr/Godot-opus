a68b8eaa8956e478cb5e4ff17ba0f95506be5c04
Mysgym <>
2024-08-24 17:57:07 +0200

    handle name consistency

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -28,7 +28,7 @@ void uninitialize_opus_module(ModuleInitializationLevel p_level){
 
 extern "C" {
 
-	GDExtensionBool GDE_EXPORT godopus_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address,
+	GDExtensionBool GDE_EXPORT godot_opus_library_init(GDExtensionInterfaceGetProcAddress p_get_proc_address,
 		   	const GDExtensionClassLibraryPtr p_library,
 			GDExtensionInitialization *r_initialization)
 	
